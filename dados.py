896a40138a6d36f45b34bff6402c34363d56bf3e
Yancel Moises Salinas <sagoyanfisic1@gmail.com>
2013-03-31 20:51:35 -0500

    Create dados.py

---
+++ dados.py
@@ -0,0 +1,16 @@
+import random
+
+while True :
+    valor = raw_input("Tirar dados(Si(1) or No(0)) : ")
+    if valor is "1" : 
+        dado=random.randint(1,6)
+        dado2=random.randint(1,6)
+        print dado 
+        print dado2
+    if dado==dado2 :
+    	print "SOn iguales"
+    elif valor is "0":   
+      print "Encontraron dados seguro :( \n Apagando...."
+      print "\nApagando..."*6
+      break	
+   
